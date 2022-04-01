df65c849756edd620ddd1379ad5c0cfaf603f737
lamhai1401 <lamthanhhai141@gmail.com>
2022-04-01 11:20:52 +0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lamhai1401/signal-elixir-v2
+
+go 1.17
+
+require github.com/quasilyte/go-ruleguard/dsl v0.3.19
---
+++ rules.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/quasilyte/go-ruleguard/dsl"
+
+func dupSubExpr(m dsl.Matcher) {
+	m.Match(`$x || $x`,
+		`$x && $x`,
+		`$x | $x`,
+		`$x & $x`).
+		Where(m["x"].Pure).
+		Report(`suspicious identical LHS and RHS`)
+}
+
+func boolExprSimplify(m dsl.Matcher) {
+	m.Match(`!($x != $y)`).Suggest(`$x == $y`)
+	m.Match(`!($x == $y)`).Suggest(`$x != $y`)
+}
